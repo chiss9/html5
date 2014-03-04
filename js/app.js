15d25f7941bbef337567f881bf4ca14f02792561
chris <chris@ckkr.net>
2014-03-04 16:14:34 -0600

    update header and title2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   console.log('here');
   $('#top_menu').on('click', 'a', function(event) {
     event.preventDefault();
+    console.log($(this));
     $('#title1')[0].innerText = $(this)[0].innerText;
+    $('#title2')[0].innerText = $(this)[0].dataset.text;
     $('#top_section').css("background", "url('" + $(this)[0].href + "') no-repeat");
     $('#top_section').css("background-size", "100% 100%");
   });
